61acd46e4eadaacb4e91b0715c1686cc9677b0d0
Brady Rohrig <brady.rohrig@gmail.com>
2022-09-10 15:52:59 -0700

    Initial commit

--- oop.rb
+++ oop.rb
@@ -2,7 +2,15 @@
 # it should have a dynamic name attribute
 # it should have a color attribute, that is silver by default
 # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
-
+class Unicorn
+    def initialize(name, say)
+    @name = name
+    @color = "silver"
+    @say = p "*~* #{say} *~*"
+   
+end
+end
+p Unicorn.new("James", "I am a unicorn")
 
 
 #  Write a class called Vampire
@@ -10,7 +18,24 @@
 #  it should have a pet attribute, that is a bat, by default BUT it could be dynamic if info is passed in initially
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
+class Vampire
+    def initialize(name, pet_type)
+        @name = name
+        @pet_type = "bat"
+        @thirsty = true
+       
+    def drink
+        @thirsty = false
+    end
+end
+end
 
+p Vampire.new("bob", "bat")
+p Vampire.new("william", "monkey")
+
+vampire1 = Vampire.new("james", "cockroach")
+vampire1.drink
+p vampire1
 
 #  Write a Dragon class
 #  it should have a dynamic name attribute (string)
@@ -18,6 +43,32 @@
 #  it should have a dynamic color attribute (string)
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
+class Dragon
+    def initialize(name, rider, color)
+        @name = name
+        @rider = rider
+        @color = color
+        @is_hungry = true
+
+        n = 0
+
+        def eat
+            n += 1
+        end
+
+        if n >= 4 then @is_hungry = false
+        end
+    end
+end
+
+dragon1 = Dragon.new("tim", "tall", "black")
+dragon1.eat
+dragon1.eat
+dragon1.eat
+dragon1.eat
+p dragon1
+
+
 
 
 #  Write a Hobbit class
@@ -28,3 +79,27 @@
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+class Hobbit
+    def initialize(name, disposition)
+        @name = name
+        @disposition = disposition
+        @age = 0
+        @is_adult = false
+        @is_old = false
+        if @name == "Frodo" then @has_ring = true else @has_ring = false end
+        i = 0
+        def celebrate_birthday
+            i += 1
+        end
+
+        if i >= 33 then @is_adult = true end
+        if i >= 101 then @is_old = true end
+end
+end
+
+sam = Hobbit.new("sam", "cheerful")
+p sam
+
+
+
+
--- self-eval.md
+++ self-eval.md
@@ -1,5 +1,8 @@
 # Self Evaluation
 
 - This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?
+I have struggled with this for 3 days now and am definitely in the "unproductive struggle" mode. Help from others and the internet is failing me because it's not the concepts that I'm struggling with, it's the basic syntax and code composition rules that I have no clue of that are messing me up.
 - How is your workflow going, in terms of taking breaks, staying focused, etc.?
+It's ok, I'm getting up and taking breaks when needed, I have much more fundamental issues with this material.
 - What do you need to do to be ready for intense technical lessons and challenges like this, for 6 hours/day next week? What support do you need?
+A little bit of coding basics. What the symbols mean, what the rules are for putting things together. I have ideas of HOW to do things, but translating the conceptual side into actual working code isn't happening, because I don't know what I'm doing at a very basic level.
